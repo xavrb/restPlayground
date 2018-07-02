4b1980fe3a7ff886cdd4ab69dac2183d79b5b5c3
xavrb <javier.mejia@protonmail.ch>
2018-07-01 23:08:53 -0500

    added a serializer

---
+++ couchtime/couchtimeProject/couchtimeapi/api/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers  
+from .models import Movie
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    """Serializer to map the Model instance into JSON format."""
+
+    class Meta:
+        """ Meta class to map serializers' fields with the model fields """
+        model = Movie
+        fields = ('id',
+        'title',
+        'genre',
+        'cast',
+        'director',
+        'year',
+        'country',
+        'comment',
+        'rating',
+        'created_at'
+        )
+        read_only_fields = ('created_at')
