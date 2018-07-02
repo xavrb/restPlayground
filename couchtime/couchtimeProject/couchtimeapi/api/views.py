233d78ec43d9fb4b0ac442f3fd314fc51c9c7864
xavrb <javier.mejia@protonmail.ch>
2018-07-01 23:32:11 -0500

    created a generic view, post

--- couchtime/couchtimeProject/couchtimeapi/api/views.py
+++ couchtime/couchtimeProject/couchtimeapi/api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import MovieSerializer
+from .models import Movie
+
+
 
 # Create your views here.
+
+
+class CreateView(generics.ListCreateAPIView):
+    """ This class defines the create behavior of our rest api """
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    
+
+    def perform_create(self, serializer):
+        """Save the post data when creating a new movie """
+        
+
+        serializer.save()
